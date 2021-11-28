7229f4a767a1df0b1097b4254c60e8bc12aa801e
Highfire1 <tseng.andersonn@gmail.com>
2021-11-27 22:32:06 -0800

    oh my god css

--- custom/shop/shop.js
+++ custom/shop/shop.js
@@ -1,36 +1,33 @@
-
-
-
-
 let items = [
-    ["ice cream sandwich", "melona.png", 1, 0],
+    ["ice cream sandwich", "sandwich.png", 1, 0],
     ["melona", "melona.png", 1, 0],
-    ["melona", "melona.png", 1, 0],
-    ["melona", "melona.png", 1, 0],
-    ["melona", "melona.png", 1, 0],
-    ["melona", "melona.png", 1, 0],
+    ["chips", "melona.png", 1, 0],
+    ["granola bar", "melona.png", 2, 0],
+    ["drink", "melona.png", 2, 0],
+    ["pricy drink", "melona.png", 2, 0],
 ]
 
 
 
 let grid = document.getElementById("grid")
 
-for (var i = items.length - 1; i >= 0; i--) {
+for (var i = 0; i <= items.length; i++) {
 
     let div = document.createElement("div")
 
     let image = document.createElement("img")
-    image.src = "assets/" + items[0][1]
+    image.src = "assets/" + items[i][1]
     div.append(image)
 
     let item = document.createElement("p")
     
-    item.textContent = items[i][0] + " $" + items[i][2]
+    item.textContent = items[i][0] + " | $" + items[i][2] + " | " + items[i][3]
     div.append(item)
-
     
+    div.id = i;
     div.addEventListener("click", function() {
-        console.log("hi")
+       items[this.id][3] += 1
+       item.textContent = items[this.id][0] + " | $" + items[this.id][2] + " | " + items[this.id][3]
     })
 
     div.classList.add("item")

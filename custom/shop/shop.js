31592564eed12e72e8db7214eb74ae69162aca75
Highfire1 <tseng.andersonn@gmail.com>
2021-11-30 10:17:29 -0800

    e

--- custom/shop/shop.js
+++ custom/shop/shop.js
@@ -112,8 +112,8 @@ reloadAll()
 
 
 let csvContent = "data:text/csv;charset=utf-8,";
-rows.forEach(function(items) {
-    let row = items.join(",");
+items.forEach(function(rowArray) {
+    let row = rowArray.join(",");
     csvContent += row + "\r\n";
 });
 var encodedUri = encodeURI(csvContent);

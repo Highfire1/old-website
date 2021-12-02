9164965f7ebb3705919ca15dcca0436344c90df9
Highfire1 <tseng.andersonn@gmail.com>
2021-12-02 02:40:24 -0800

    more testing

--- js/blog.js
+++ js/blog.js
@@ -84,7 +84,7 @@ function populatePost(url) {
     xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if(xhr.readyState == 4 && xhr.status == 200) {
-            document.getElementById('content').innerHTML = xhr.responseText;
+            document.getElementById('postcontent').innerHTML = xhr.responseText;
         }
     }
     xhr.open('GET', url, true);
